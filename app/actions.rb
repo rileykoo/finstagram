42fea7f6965112205a40076e10c25b253a40550e
rileykoo <rileym1206@gmail.com>
2022-04-03 22:34:34 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -7,6 +7,7 @@ end
 
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  @current_user = User.find_by(id: session[:user_id])
   erb(:index) 
 end
 
@@ -54,4 +55,26 @@ end
 get '/logout' do
   session[:user_id] = nil
   redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+  if @finstagram_post.save
+    redirect(to('/'))
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])
+  erb(:"finstagram_posts/show")
 end
